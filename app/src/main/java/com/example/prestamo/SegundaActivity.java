7f06143c2d6fdd81104725d5095e488ebf046659
Jonnahbf <jonathan.ochoa15@est.unanleon.edu.ni>
2019-04-09 16:58:10 -0600

    Proyecto Terminado

--- app/src/main/java/com/example/prestamo/SegundaActivity.java
+++ app/src/main/java/com/example/prestamo/SegundaActivity.java
@@ -10,9 +10,12 @@ import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import java.net.Inet4Address;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class SegundaActivity extends AppCompatActivity {
@@ -22,18 +25,22 @@ public class SegundaActivity extends AppCompatActivity {
     private EditText monto;
     private EditText plazo;
     private TextView cuota;
+    private TextView fecha_fin;
+    private String date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
-        String date = new SimpleDateFormat("dd/MM/yyyy").format(new Date());
-        TextView fecha = findViewById(R.id.textView_fecha);
-        plazo = findViewById(R.id.editText_plazo);
-        monto = findViewById(R.id.editText_monto);
-        monto_a_pagar = findViewById(R.id.textViewMontoPagar);
-        interes = findViewById(R.id.spinner_interes);
-        cuota = findViewById(R.id.textViewMontoCuota);
+        date = new SimpleDateFormat("dd/MM/yyyy").format(new Date());
+        TextView fecha = findViewById(R.id.textView_fecha); //Obtenemos el TextView de fecha
+        plazo = findViewById(R.id.editText_plazo); //Obtenemos el EditText de plazo
+        monto = findViewById(R.id.editText_monto); //Obtenemos el EditText de monto
+        monto_a_pagar = findViewById(R.id.textViewMontoPagar); //Obtenemos el TextView de Monto a pagar
+        interes = findViewById(R.id.spinner_interes); //Obtenemos el spinner de interes
+        fecha_fin = findViewById(R.id.textView_fechaFin); //Obtenemos el TextView de fecha fin
+        cuota = findViewById(R.id.textViewMontoCuota); //Obtenemos el TextView de cuota
         fecha.setText(date);
+       fecha_de_pago();
         plazo.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -42,7 +49,9 @@ public class SegundaActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+
                 pago();
+                fecha_de_pago();
             }
 
             @Override
@@ -71,23 +80,6 @@ public class SegundaActivity extends AppCompatActivity {
             }
         });
 
-        plazo.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                pago();
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
         interes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -103,26 +95,53 @@ public class SegundaActivity extends AppCompatActivity {
 
 
     public void pago(){
-        int aux = 0;
-        if(monto.getText().toString().length()!=0)
+        int aux = 0; //Variable auxiliar que almacenara el valor ingresado en monto
+        if(monto.getText().toString().length()!=0) //Verificamos que el EditText no este en 0
             aux=Integer.valueOf(monto.getText().toString());
-        int aux_interes = Integer.valueOf(interes.getSelectedItem().toString());
-        //Log.d("interes", interes.getSelectedItem().toString());
-        Double total = Double.valueOf(aux + ((aux*aux_interes)/100));
-        String monto_string = String.valueOf(total);
-        monto_a_pagar.setText(monto_string);
-        Double aux_plazo = 0.0;
-        if(plazo.getText().toString().length()!=0){
-            int plazo_aux = Integer.valueOf(plazo.getText().toString());
-            aux_plazo = Double.valueOf((total) / (plazo_aux));
-            cuota.setText(String.valueOf(aux_plazo));
+        int aux_interes = Integer.valueOf(interes.getSelectedItem().toString()); //Variable auxiliar que almacenara el interes que se le aplicara al monto
+        int plazo_interes = 0;
+        if(plazo.getText().length()!=0){
+            plazo_interes = Integer.valueOf(plazo.getText().toString());
+        }
+        int interes_final = ((aux*aux_interes)/100) * plazo_interes;
+        Double total = Double.valueOf(aux + interes_final); //Creamos una variable que almacenara el monto total ya con el interes calculado
+        String monto_string = String.valueOf(total); //Convertimos el total a string para poder imprimirlo en un TextView
+        monto_a_pagar.setText(monto_string); //Escribimos el total ya convertido a String en el TextView
+        Double aux_plazo = 0.0; //Definimos una variable Double que almacenara la cantidad de dinero que tendra que abonar el cliente por mes
+        if(plazo.getText().toString().length()!=0){ //Verificamos que el plazo no sea 0
+            int plazo_aux = Integer.valueOf(plazo.getText().toString()); //Obtenemos el plazo que se ha escogido en el spinner
+            aux_plazo = Double.valueOf((total) / (plazo_aux)); //Dividimos el total/plazo para obtener las cuotas que dara el cliente mensual
+            cuota.setText(String.valueOf(aux_plazo)); //Mostramos el valor del plazo en un TextView
 
         }
         else{
-            cuota.setText(String.valueOf(total));
+            cuota.setText(String.valueOf(total)); //Si el plazo es 0, el cliente tendra que pagar el total en una sola cuota
         }
+    }
 
-
+    public void fecha_de_pago(){
+        Calendar fechaActual = Calendar.getInstance();
+        String fecha_hoy;
+        int año = fechaActual.get(Calendar.YEAR);
+        int mes = fechaActual.get(Calendar.MONTH);
+        mes++;
+        int aux;
+        int dia = fechaActual.get(Calendar.DAY_OF_MONTH);
+        if(plazo.getText().toString().isEmpty()){
+            mes++;
+            fecha_hoy = (String.valueOf(dia)) + "/" + (String.valueOf(mes)) + "/" + (String.valueOf(año));
+            fecha_fin.setText(fecha_hoy);
+        }
+        else{
+            aux = Integer.valueOf(plazo.getText().toString());
+            fechaActual.add(Calendar.MONTH, aux);
+            año = fechaActual.get(Calendar.YEAR);
+            mes = fechaActual.get(Calendar.MONTH);
+            mes++;
+            dia = fechaActual.get(Calendar.DAY_OF_MONTH);
+            fecha_hoy = (String.valueOf(dia)) + "/" + (String.valueOf(mes)) + "/" + (String.valueOf(año));
+            fecha_fin.setText(fecha_hoy);
+        }
 
     }
 
